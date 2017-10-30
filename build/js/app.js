1f9f792ebeca4b31a04eca8a1ce97a7040c42a69
SC <csafi13@yahoo.com>
2017-10-29 21:26:13 -0700

    show list of doctors after API call.

--- build/js/app.js
+++ build/js/app.js
@@ -1,5 +1,8 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
+ exports.apiKey = "be56d99e25fceebd5df712622dc97243";
+
+},{}],2:[function(require,module,exports){
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
@@ -9,7 +12,7 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-// var apiKey = require('./../.env').apiKey;
+var apiKey = require('./../.env').apiKey;
 
 var Doctor = exports.Doctor = function () {
   function Doctor() {
@@ -17,20 +20,17 @@ var Doctor = exports.Doctor = function () {
   }
 
   _createClass(Doctor, [{
-    key: "listDoctorsByCondition",
-    value: function listDoctorsByCondition(condition, location) {
-      $('.showDoctors').append("test");
+    key: 'getDoctors',
+    value: function getDoctors(condition) {
+      $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + condition + '&location=wa-seattle&skip=0&limit=10&user_key=' + apiKey).then(function (response) {
+        var doctors = response;
+        doctors.data.forEach(function (doctor) {
+          $('.showDoctors').append('\n          <p>Name: ' + doctor.profile.first_name + ' ' + doctor.profile.last_name + '</p>\n          <p>Address: ' + doctor.practices[0].visit_address.street + '<br>\n          ' + doctor.practices[0].visit_address.city + ', ' + doctor.practices[0].visit_address.state + ',\n          ' + doctor.practices[0].visit_address.zip + '</p>\n          <p>Phone: ' + doctor.practices[0].phones[0].number + '</p>\n          <p>Website: ' + doctor.practices[0].website + '</p>\n          <p>Accepting New Patients: ' + doctor.practices[0].accepts_new_patients + '</p>');
+        });
+      }).fail(function (error) {
+        $('.showErrors').text('There was an error processing your request: ' + error.responseText + '. Please try again.');
+      });
     }
-    // listDoctorsByCondition(condition, location) {
-    //   $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=${location}&skip=0&limit=10&user_key=be56d99e25fceebd5df712622dc97243`).then(function(response) {
-    // 'data' is retreived from API array
-    // let list = response.data;
-    // list.forEach(function(doctor) {
-    //   $('.showDoctors').append('<li>' + doctor.data.practices.name + '</li>');
-    // });
-    // });
-    // }
-
   }]);
 
   return Doctor;
@@ -38,24 +38,29 @@ var Doctor = exports.Doctor = function () {
 
 // this file is to make API request and display results
 
-},{}],2:[function(require,module,exports){
+},{"./../.env":1}],3:[function(require,module,exports){
 'use strict';
 
 var _doctor = require('./../js/doctor.js');
 
 $(document).ready(function () {
-  $('#conditionSearch').submit(function (event) {
+  $('form#doctorSearch').submit(function (event) {
     event.preventDefault();
-    var doctor = new _doctor.Doctor();
     var condition = $('#condition').val();
-    var location = $('#location').val();
-    $('#condition').val("");
-    $('#location').val("");
+    var doctorName = $('#doctorName').val();
+    var searchDoctor = new _doctor.Doctor();
+    // $('#condition').val("");
+    // $('#doctorName').val("");
 
-    doctor.listDoctorsByCondition(condition, location);
+    // if(condition != "") {
+    // $('.showDoctors').removeClass('hide');
+    //   $('.showDoctors').append(`First Name: ${searchDoctor.getDoctors.first_name}`);
+    // }
+
+    searchDoctor.getDoctors(condition);
   });
 });
 
 // this file is to receive user inputs
 
-},{"./../js/doctor.js":1}]},{},[2]);
+},{"./../js/doctor.js":2}]},{},[3]);
--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -6,10 +6,15 @@ $(document).ready(function() {
     let condition = $('#condition').val();
     let doctorName = $('#doctorName').val();
     let searchDoctor = new Doctor();
-    searchDoctor.getDoctor(condition);
-    $('#condition').val("");
-    $('#doctorName').val("");
+    // $('#condition').val("");
+    // $('#doctorName').val("");
 
+    // if(condition != "") {
+      // $('.showDoctors').removeClass('hide');
+    //   $('.showDoctors').append(`First Name: ${searchDoctor.getDoctors.first_name}`);
+    // }
+
+    searchDoctor.getDoctors(condition);
   });
 });
 
